25246b3bb921e2d560d142e84f60ffbaeb734c09
Roger <94183267+ChanKinTsun@users.noreply.github.com>
2023-11-20 03:51:02 +0800

    Add files via upload

---
+++ S381F-Project-To-Do List Application/README.md
@@ -0,0 +1,101 @@
+# To-Do List Application
+
+Group: 22
+Name: 
+Chan Kin Tsun (13395240)
+
+Application link: https://projectsample381.render.com/
+
+# Intro
+
+Welcome to the ToDo List App! This simple app allows you to manage your tasks and keep track of completed items. Below, you'll find information on how to use the app and test various functionalities.
+
+# Login/Logout Pages
+
+1. Access the login page: http://localhost:3000/login.
+
+2. You'll be redirected to the login page, where you can enter your credentials.
+
+3. Valid Login Information
+	Use the following credentials to log in:
+
+	1. Username: guest1 Password: guest
+
+	2. Username: guest2 Password: guest
+
+4. Logging Out
+
+	To log out, click to logout button. You will be redirected to the home page after successfully logging out.
+
+
+# CRUD Services
+	
+1. Adding a Task
+	
+	On the ToDo list page http://localhost:3000/todo, you can see your existing tasks.
+
+	Click on the "Add Task" link to navigate to the add task page.
+
+	Enter a new task in the input field and click the "+" button to add it to your ToDo list.
+
+2. Completing a Task
+
+	On the ToDo list page http://localhost:3000/todo, you can see your tasks.
+
+	Check the checkbox next to the task you want to mark as completed.
+
+	Click on the "Complete" button to move the selected task to the completed tasks list.
+
+3. Viewing Completed Tasks
+
+	On the ToDo list page http://localhost:3000/todo, click on the "Completed task" link to view completed tasks.
+
+	On the completed tasks page, you can see the list of completed tasks.
+	
+4. Deleting Completed Tasks
+
+	On the completed tasks page http://localhost:3000/completetask, check the checkbox next to the completed tasks you want to delete.
+
+	Click on the "Delete" button to remove the selected completed tasks.
+
+# API Endpoints
+
+1. Login:
+
+	Endpoint: /login
+	HTTP Method: POST
+	Parameters: name, password
+
+2. Logout:
+
+	Endpoint: /logout
+	HTTP Method: GET
+
+3. Add Task:
+
+	Endpoint: /addtask
+	HTTP Method: POST
+	Parameters: newtask
+
+4. Remove Task:
+
+	Endpoint: /removetask
+	HTTP Method: POST
+	Parameters: check (task to be removed)
+
+5. View ToDo List:
+
+	Endpoint: /todo
+	HTTP Method: GET
+
+6. View Completed Tasks:
+
+	Endpoint: /completetask
+	HTTP Method: GET
+
+7. Delete Completed Tasks:
+
+	Endpoint: /deletecompleted
+	HTTP Method: POST
+	Parameters: deleteCheck (completed task to be deleted)
+
---
+++ S381F-Project-To-Do List Application/server.js
@@ -0,0 +1,136 @@
+const assert = require('assert');
+const express = require('express');
+const session = require('cookie-session');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+const { MongoClient } = require("mongodb");
+const ObjectID = require('mongodb').ObjectID;
+const mongourl = 'mongodb+srv://rogerchan:12345678abc@cluster0.nkfu5zj.mongodb.net/?retryWrites=true&w=majority';
+const dbName = 'project'
+
+const client = new MongoClient(mongourl, {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+});
+
+try {
+   client.connect(err => {
+      const db = client.db(dbName)
+
+      db.command({ ping: 1 }, () => {
+         client.close(() => console.log(`connected to Mongodb server.`))
+      })
+   })
+} catch (err) {
+   console.error(err)
+}
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine','ejs');
+
+app.use(session({
+  secret: 'secret-key',
+  resave: false,
+  saveUninitialized: true
+}));
+
+const users = new Array(
+{name: 'guest1', password: 'guest'},
+{name: 'guest2', password: 'guest'}
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req,res) => {
+console.log(req.session);
+if (!req.session.authenticated) {
+res.redirect('/login');
+} else {
+res.status(200).render('todo',{name:req.session.name});
+}
+});
+
+app.get('/login', (req,res) => {
+res.status(200).render('login',{});
+});
+
+app.post('/login', (req, res) => {
+  users.forEach((user) => {
+    if (user.name == req.body.name && user.password == req.body.password) {
+      req.session.authenticated = true;
+      req.session.username = req.body.name;
+      res.redirect('/todo');
+    }
+  });
+});
+
+
+app.get('/logout', (req,res) => {
+req.session = null;
+res.redirect('/');
+});
+
+app.get('/back', (req,res) => {
+req.session = null;
+res.redirect('/todo');
+});
+
+var task = ["finish assignment", "do revision on s381f"];
+
+app.get("/todo", (req, res) => {
+    res.status(200).render('todo', { name: req.session.username, t: task, c: complete });
+});
+
+app.get("/addtask", (req, res) => {
+    res.status(200).render('addtask', { name: req.session.username, t: task, c: complete });
+});
+
+app.get("/completetask", (req, res) => {
+    res.status(200).render('complete', { name: req.session.username, t: task, c: complete });
+});
+
+app.post('/addtask', (req, res) => {
+    var newTask = req.body.newtask;
+    task.push(newTask);
+    res.redirect("/addtask");
+});
+
+var complete = ["finish s381f project"];
+
+app.post("/completetask", (req, res) => {
+   var completeTask = req.body.check;
+
+   if (typeof completeTask === "string") {
+      complete.push(completeTask);
+      task.splice(task.indexOf(completeTask), 1);
+   } else if (Array.isArray(completeTask)) {
+      completeTask.forEach((item) => {
+         complete.push(item);
+         task.splice(task.indexOf(item), 1);
+      });
+   }
+
+   res.redirect("/todo");
+});
+
+
+app.post("/deletecompleted", (req, res) => {
+    var deleteCheck = req.body.deleteCheck;
+    if (typeof deleteCheck === "string") {
+        complete.splice(complete.indexOf(deleteCheck), 1);
+    } else if (typeof deleteCheck === "object") {
+        for (var i = 0; i < deleteCheck.length; i++) {
+            complete.splice(complete.indexOf(deleteCheck[i]), 1);
+        }
+    }
+    res.redirect("/completetask");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
